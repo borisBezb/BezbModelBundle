891420b6f2d8f4d03b3b4f2100739867483fd079
borisbezb <wc3inside@gmail.com>
2018-01-19 01:19:58 +0800

    Behavior compiler pass added

--- BezbModelBundle.php
+++ BezbModelBundle.php
@@ -2,10 +2,8 @@
 
 namespace Bezb\ModelBundle;
 
-use Bezb\ModelBundle\Component\BehaviorInterface;
-use Bezb\ModelBundle\Component\ScenarioInterface;
-use Bezb\ModelBundle\DependencyInjection\BehaviorCompilerPass;
-use Bezb\ModelBundle\DependencyInjection\ScenarioCompilerPass;
+use Bezb\ModelBundle\Component\{ BehaviorInterface, ScenarioInterface };
+use Bezb\ModelBundle\DependencyInjection\{ BehaviorCompilerPass, ScenarioCompilerPass };
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -16,13 +14,13 @@ class BezbModelBundle extends Bundle
         parent::build($builder);
 
         $builder->addCompilerPass(new ScenarioCompilerPass());
+        $builder->addCompilerPass(new BehaviorCompilerPass());
 
         $builder->registerForAutoconfiguration(ScenarioInterface::class)
             ->addTag('model.scenario')
             ->setPublic(true);
 
         $builder->registerForAutoconfiguration(BehaviorInterface::class)
-            ->addTag('model.behavior')
-            ->setPublic(false);
+            ->addTag('model.behavior');
     }
 }
